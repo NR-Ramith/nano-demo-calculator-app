ace282a3d9f418baa38dcfa628ae0b7a1c4c19aa
Ramith <Natakalaram1@gmail.com>
2023-08-23 14:32:36 +0530

    Server updated

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ def add():
     first = data.get('first', 0)
     second = data.get('second', 0)
     result = first + second
-    return jsonify({"result": result}), 20
+    return jsonify({"result": result}), 200
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
